7eea4a1b5d5e3146b53575af572860fa05254fe1
Wiktor Wolak <ruwi@o2.pl>
2012-05-09 23:03:30 +0200

    parser działa: wypisuje na stdout dane o stronach [pod]kategorii

--- category.py
+++ category.py
@@ -1,25 +1,77 @@
 #-*- coding: utf-8 -*-
-
+import sys
+import urllib2 as url
 from HTMLParser import HTMLParser
 
 class CategoryParser(HTMLParser):
-	def __init__(self,*args,**argv):
+	def init(self,*args,**argv):
+		self.p = 0
+		if 'p' in args:
+			self.p = 1
+		self.clean()
+		
+	def clean(self):
 		self.n = 0
-		HTMLParser(self,*args,**argv)
+		self.lvl = -1
+		self.www = ''
+		self.title = ''
+		self.quantity = ''
 		
 	def handle_starttag(self,tag,attrs):
 		for i in range(1,5):
-			if tag == 'a' and ('class', "alleLink lvl" + str(i)) in attrs:
+			if (tag == 'a') and (('class',
+					"alleLink lvl" + str(i)) in attrs):
 				self.n=1
 				self.lvl=i
+				self.www = [i[1] for i in attrs
+						if i[0]=='href'][0]
+		q =[i[1] for i in attrs	if i[0] =='class']
+		if q:
+			if (tag == 'span') and ("small" in q[0]):
+				self.n=2
 
 	def handle_endtag(self,tag):
 		if self.n == 1 and tag == 'a':
 			self.n = 0
-			self.lvl = -1
+		if self.n == 2 and tag == 'span':
+			self.n = 0
+		if tag == 'div' and self.www:
+			self.push_to_db()
 
 	def handle_data(self,data):
 		if self.n == 1:
 			self.title = data
-		if self.n == 2:
-			self.quantity
+		elif self.n == 2:
+			try:
+				self.quantity = data[1:-1]
+			except:
+				print "error: niewłaściwy format: '"+data+''
+				print "self.title",self.title
+				print "self.www",self.www
+				sys.exit(2)
+				self.quality = data 
+
+	def push_to_db(self):
+		if self.p:
+			#wypisanie danych
+			print 'lvl=      ',self.lvl
+			print 'www=      ',self.www
+			print 'title=    ',self.title
+			print 'quantity= ',self.quantity
+			print ''
+
+		else:
+			pass #TODO: zapis do db
+		self.clean()
+			
+
+www = 'http://allegro.pl/category_map.php'
+try:
+	s = url.urlopen(www).read()
+except:
+	print "błąd przy pobieraniu głównego źródła"
+	sys.exit(1)
+c = CategoryParser()
+c.init('p')
+c.feed(s)
+
